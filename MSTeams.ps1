c3d7af31d9987aa7936fcf18a78e9152bac38d45
Bruce Jernell <59374529+opconWizard@users.noreply.github.com>
2020-06-23 11:28:18 -0500

    Add files via upload

---
+++ MSTeams.ps1
@@ -0,0 +1,12 @@
+﻿param(
+    $teamsURL # URL generated after creating Incoming Webhook
+    ,$message # Message to send to Teams
+)
+try
+{ Invoke-RestMethod -URI "$teamsURL" -Method POST -Body ('{"text":"' + $message + '"}') -ContentType "application/json" }
+catch [Exception]
+{ 
+    Write-Host $_ 
+    Write-Host $_.Exception.Message
+    Exit 1
+}
